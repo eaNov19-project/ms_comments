0ab415b5a51e36c0acc8c1ddf9f6b2e009128e35
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-16 17:32:42 -0600

    Dynamic feign configuration

--- src/main/java/ea/sof/ms_comments/service/AuthService.java
+++ src/main/java/ea/sof/ms_comments/service/AuthService.java
@@ -3,10 +3,7 @@ package ea.sof.ms_comments.service;
 import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "svc-bank", url = "http://104.154.33.123:8080/auth")
+@FeignClient(name = "${feign.name}", url = "${feign.url}")
 public interface AuthService extends MsAuthShowcase {
-/*
-    @GetMapping("/validate-token")
-    Response isValidToken(@RequestHeader("Authorization") String jwt);*/
 
 }
