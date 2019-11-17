dcaeb4595ca3cb85d9e72506675e9ad48b234d1a
islam ahmad <islam.ahmad@gmail.com>
2019-11-17 11:08:28 -0600

    updated k8s files updated application.properties updated authservice.java with k8s variables

--- src/main/java/ea/sof/ms_comments/service/AuthService.java
+++ src/main/java/ea/sof/ms_comments/service/AuthService.java
@@ -3,7 +3,10 @@ package ea.sof.ms_comments.service;
 import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "${feign.name}", url = "${feign.url}")
+//@FeignClient(name = "${feign.name}", url = "${feign.url}")
+@FeignClient(name="authService", url = "${AUTHENTICATE_SERVICE}")
+/*authService is the name of the object autowired
+in the contoller using this client*/
 public interface AuthService extends MsAuthShowcase {
 
 }
