002863c02dc59ddf4be9d4a1d50af812efabf5f1
rustem bayetov <rustem.bayetov@gmail.com>
2019-11-18 20:40:56 -0600

    properties changes

--- src/main/java/ea/sof/ms_comments/service/AuthService.java
+++ src/main/java/ea/sof/ms_comments/service/AuthService.java
@@ -4,7 +4,7 @@ import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
 //@FeignClient(name = "${feign.name}", url = "${feign.url}")
-@FeignClient(name="authService", url = "${AUTHENTICATE_SERVICE}")
+@FeignClient(name="authService", url = "${authenticate.service}")
 /*authService is the name of the object autowired
 in the contoller using this client*/
 public interface AuthService extends MsAuthShowcase {
